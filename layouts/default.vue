d57b7da37d980ea2ebb29e4c18b9af120d821403
kirill <kirillkaliuzhny1@gmail.com>
2024-04-20 11:32:36 +0300

    start work

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<script setup>
+    import '@/assets/css/global.css'
+</script>
+
+<template>
+    <div class="wrapper">
+        <header></header>
+        <main class="main">
+            <slot />
+        </main>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
